5868fcab1a8abcbbbb350260967f135d289764bf
Abel Gaming <lcpdpolice4939@gmail.com>
2021-04-26 04:26:05 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,74 @@
+Config = {}
+
+-- These are all the items you can purcahse
+Config.Items = {
+    {
+        label = "Pistol Ammo (100 Rounds)",
+        item = "WEAPON_PISTOL",
+        price = 250
+    },
+    {
+        label = "Stun Cartridge (10 Rounds)",
+        item = "WEAPON_STUNGUN",
+        price = 250
+    },
+    {
+        label = "Micro SMG Ammo (100 Rounds)",
+        item = "WEAPON_MICROSMG",
+        price = 250
+    },
+    {
+        label = "Shotgun Ammo (100 Rounds)",
+        item = "WEAPON_PUMPSHOTGUN",
+        price = 250
+    },
+    {
+        label = "Assult Rifle Ammo (100 Rounds)",
+        item = "WEAPON_ASSAULTRIFLE",
+        price = 250
+    },
+    {
+        label = "Special Carbine Ammo (100 Rounds)",
+        item = "WEAPON_SPECIALCARBINE",
+        price = 250
+    },
+    {
+        label = "Sniper Ammo (100 Rounds)",
+        item = "WEAPON_SNIPERRIFLE",
+        price = 250
+    },
+    {
+        label = "Fireworks (100)",
+        item = "WEAPON_FIREWORK",
+        price = 250
+    },
+    {
+        label = "Grenades (10 Count)",
+        item = "WEAPON_GRENADE",
+        price = 250
+    },
+    {
+        label = "Tear Gas (10 Count)",
+        item = "WEAPON_BZGAS",
+        price = 250
+    },
+    {
+        label = "Smoke Grenade (10 Count)",
+        item = "WEAPON_SMOKEGRENADE",
+        price = 250
+    },
+
+}
+
+-- Ammunation Locations
+Config.Locations = {
+    { ['x'] = 844.84,  ['y'] = -1029.57,  ['z'] = 28.19},
+    { ['x'] = -664.7,  ['y'] = -939.22,  ['z'] = 21.83},
+    { ['x'] = 812.62,  ['y'] = -2153.63,  ['z'] = 29.62},
+    { ['x'] = 1694.55,  ['y'] = 3755.55,  ['z'] = 34.71},
+    { ['x'] = -329.6,  ['y'] = 6079.57,  ['z'] = 31.45},
+    { ['x'] = 249.69,  ['y'] = -46.52,  ['z'] = 69.94},
+    { ['x'] = 18.47,  ['y'] = -1109.81,  ['z'] = 29.8},
+    { ['x'] = 2570.34,  ['y'] = 297.98,  ['z'] = 108.73},
+    { ['x'] = -1117.16,  ['y'] = 2694.18,  ['z'] = 18.55}
+}
